ae372eb19e1d51f4e8963d317aed5004e0fffae1
Arjan Zuidhof <kommaardoormetdiepost@gmail.com>
2025-09-08 14:32:30 +0200

    partial commit 1

---
+++ test/aardbevingen-groningen/layers/layers.js
@@ -0,0 +1,49 @@
+var wms_layers = [];
+
+        var lyr_StamenTonerLight_0 = new ol.layer.Tile({
+            'title': 'StamenTonerLight_0',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: '<a href=""></a>',
+                url: 'http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png'
+            })
+        });var format_nietvoelbarebevingen_1 = new ol.format.GeoJSON();
+var features_nietvoelbarebevingen_1 = format_nietvoelbarebevingen_1.readFeatures(json_nietvoelbarebevingen_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_nietvoelbarebevingen_1 = new ol.source.Vector({
+    attributions: '<a href=""></a>',
+});
+jsonSource_nietvoelbarebevingen_1.addFeatures(features_nietvoelbarebevingen_1);var lyr_nietvoelbarebevingen_1 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_nietvoelbarebevingen_1, 
+                style: style_nietvoelbarebevingen_1,
+                title: '<img src="styles/legend/nietvoelbarebevingen_1.png" /> niet voelbare bevingen'
+            });var format_voelbarebevingen_2 = new ol.format.GeoJSON();
+var features_voelbarebevingen_2 = format_voelbarebevingen_2.readFeatures(json_voelbarebevingen_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_voelbarebevingen_2 = new ol.source.Vector({
+    attributions: '<a href=""></a>',
+});
+jsonSource_voelbarebevingen_2.addFeatures(features_voelbarebevingen_2);var lyr_voelbarebevingen_2 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_voelbarebevingen_2, 
+                style: style_voelbarebevingen_2,
+                title: 'voelbare bevingen'
+            });
+
+lyr_StamenTonerLight_0.setVisible(true);lyr_nietvoelbarebevingen_1.setVisible(true);lyr_voelbarebevingen_2.setVisible(true);
+var layersList = [lyr_StamenTonerLight_0,lyr_nietvoelbarebevingen_1,lyr_voelbarebevingen_2];
+lyr_nietvoelbarebevingen_1.set('fieldAliases', {'Datum': 'Datum', 'Locatie': 'Locatie', 'LAT': 'LAT', 'LON': 'LON', 'Magnitude': 'Magnitude', });
+lyr_voelbarebevingen_2.set('fieldAliases', {'Datum': 'Datum', 'Locatie': 'Locatie', 'LAT': 'LAT', 'LON': 'LON', 'Magnitude': 'Magnitude', });
+lyr_nietvoelbarebevingen_1.set('fieldImages', {'Datum': 'TextEdit', 'Locatie': 'TextEdit', 'LAT': 'TextEdit', 'LON': 'TextEdit', 'Magnitude': 'TextEdit', });
+lyr_voelbarebevingen_2.set('fieldImages', {'Datum': 'DateTime', 'Locatie': 'TextEdit', 'LAT': 'TextEdit', 'LON': 'TextEdit', 'Magnitude': 'TextEdit', });
+lyr_nietvoelbarebevingen_1.set('fieldLabels', {'Datum': 'inline label', 'Locatie': 'inline label', 'LAT': 'no label', 'LON': 'no label', 'Magnitude': 'header label', });
+lyr_voelbarebevingen_2.set('fieldLabels', {'Datum': 'inline label', 'Locatie': 'inline label', 'LAT': 'no label', 'LON': 'no label', 'Magnitude': 'header label', });
+lyr_voelbarebevingen_2.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
+
+OpenLayers.Renderer.SVG.prototype.MAX_PIXEL = Number.MAX_VALUE;
---
+++ test/aardbevingen-groningen/styles/voelbarebevingen_2_style.js
@@ -0,0 +1,48 @@
+var size = 0;
+var placement = 'point';
+
+var style_voelbarebevingen_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+	var magnitude = feature.get("Magnitude");
+	radiusFactor = 5; //AJZ vermenigvuldigingsfactor voor straal bevingsbol
+	var jaar = feature.get("Datum");
+	var jaar = jaar.substring(0,4);
+	if (magnitude > 3) labelText = "3+"; //AJZ tekst voor sterkste bevingen
+	var labelFont = "15.4px \'MS Shell Dlg 2\', sans-serif"; //AJZ groter lettertype
+    var labelText = "";
+    if ("" !== null) {
+        labelText = String("");
+    }
+    size = 0;
+    var labelFont = "10.4px \'MS Shell Dlg 2\', sans-serif";
+    var labelFill = "rgba(0, 0, 0, 1)";
+    var bufferColor = "";
+	var bufferWidth = 1; //AJZ, 1 geeft dikkere letter
+	var fillColor = 'rgba(255,1,1,'; // opacity fillColor en strokeColor afhankelijk van datum
+	var strokeColor = 'rgba(35,35,35,';
+	var opacity =  (jaar - 1986) / 32 //AJZ transformeert opacity tussen 0 voor begin waarnemingen en 1 huidig jaar.
+	if (jaar < 2014) opacity -= 0.2; //HACK voor sneller vervagen van oudere bevingen
+	if (jaar < 2006) opacity -= 0.2;
+	if (opacity < 0) opacity=0; // maar opacity mag niet kleiner worden dan 0 
+	if (magnitude >= 3) // AJZ aanpassingen voor 3+ bolletjes
+	{
+		labelText = "3+";
+		fillColor = 'rgba(202,0,0,';
+		radiusFactor = 7;
+		opacity += 0.3; //AJZ en weer iets donkerder maken
+	}
+	fillColor += opacity += ")"; //AJZ toevoegen opacity aan fillColor
+	strokeColor += opacity; //AJZ toevoegen opacity aan strokeColor
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: radiusFactor * magnitude,
+            stroke: new ol.style.Stroke({color: strokeColor, lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: fillColor})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
